f62589dd4410899e2f3c081360f964d66266d4a6
Thomas Burnett-Taylor <tmfksoft@gmail.com>
2023-01-31 16:40:37 +0000

    feat: Added initial files

---
+++ LabelInterop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using bpac;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = String.Join(" ", args);
+        Console.WriteLine(path);
+        bpac.DocumentClass doc = new DocumentClass();
+        if (doc.Open(path) != false)
+        {
+            
+            doc.StartPrint("", PrintOptionConstants.bpoDefault);
+            Boolean res = doc.PrintOut(1, PrintOptionConstants.bpoDefault);
+            if (!res)
+            {
+                doc.Close();
+                System.Environment.Exit(doc.ErrorCode); // Forwards on the BPAC Error Code
+            }
+            doc.EndPrint();
+            doc.Close();
+        } else
+        {
+            System.Environment.Exit(doc.ErrorCode);
+        }
+    }
+}
